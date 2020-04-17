4d80244cc0b78e6a12b93e165ca374c4e6cadb3d
julian lazaras <lazaras@pdx.edu>
2020-04-17 01:31:12 -0700

    removed object files

--- pkg-config/target/debug/deps/pkg_config-50e08c27400c6502.d
+++
@@ -1,7 +0,0 @@
-/home/cardinal/cbuild/pkg-config/target/debug/deps/pkg_config-50e08c27400c6502.rmeta: src/lib.rs
-
-/home/cardinal/cbuild/pkg-config/target/debug/deps/libpkg_config-50e08c27400c6502.rlib: src/lib.rs
-
-/home/cardinal/cbuild/pkg-config/target/debug/deps/pkg_config-50e08c27400c6502.d: src/lib.rs
-
-src/lib.rs:
--- pkg-config/target/debug/libpkg_config.d
+++
@@ -1,1 +0,0 @@
-/home/cardinal/cbuild/pkg-config/target/debug/libpkg_config.rlib: /home/cardinal/cbuild/pkg-config/src/lib.rs
